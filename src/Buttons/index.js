bf7c393955c16fe41a17f9023b45e93bfa6ffacf
Jakub Szczechowicz <jakub.szczechowicz@gmail.com>
2022-01-31 17:43:55 +0100

    React.Fragment <></> in Buttons module

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,16 +1,18 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
-    tasks.length > 0 && (
-        <div className="form__buttons">
-            <button className="form__button">
-                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone</button>
-            <button
-                className="form__button"
-                disabled={tasks.every(({ done }) => done)}>
-                Ukończ wszystkie
-            </button>
-        </div>
-));
+    <div className="form__buttons">
+    {tasks.length > 0 && (
+        <>
+        <button className="form__button">
+            {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone</button>
+        <button
+            className="form__button"
+            disabled={tasks.every(({ done }) => done)}>
+            Ukończ wszystkie
+        </button>
+        </>)}
+    </div>
+);
 
 export default Buttons;

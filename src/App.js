d1be9910334a24ed252ee813a5d864909514434d
Jakub Szczechowicz <jakub.szczechowicz@gmail.com>
2022-06-03 21:58:30 +0200

    App is now react-router ready, has subpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Author from "./features/author/Author";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import { StyledNavLink } from "./styled";
 
-const App = () => <Tasks />;
+const App = () => (
+    <BrowserRouter basename="/todo-list-react">
+        <nav>
+            <ul>
+                <li><StyledNavLink to="/zadania">Zadania</StyledNavLink></li>
+                <li><StyledNavLink to="/author">O Autorze</StyledNavLink></li>
+            </ul>
+        </nav>
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage/>
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/author">
+                <Author />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania"/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+);
 export default App;
